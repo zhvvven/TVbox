6325d6ecbdef34e36cd54206e5f1af9c853f60b5
zhvvven <zhvvven@gmail.com>
2024-11-05 15:44:39 +0800

    Update CloudflareST.sh

--- CloudflareST.sh
+++ CloudflareST.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pkg update
-pkg install wget -y
 wget -N https://ghp.ci/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.2.5/CloudflareST_linux_arm64.tar.gz
 tar -zxf CloudflareST_linux_arm64.tar.gz
 mv CloudflareST cfst
